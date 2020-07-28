eae28f6b72753bae649cae0366c40f453b153a0a
Snwblnd <33382402+Snwblnd@users.noreply.github.com>
2020-07-28 23:26:29 +0200

    Reduced Panel Distance

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+
+    protected Transform _XForm_Camera;
+    protected Transform _XFORM_Parent;
+
+    protected Vector3 LocalRotation;
+    protected float _CameraDistance = 10f;
+
+    public float MouseSensivity = 4f;
+    public float ScrollSensivity = 2f;
+    public float OrbitDampening = 10f;
+    public float ScrollDampening = 6f;
+
+    public bool CameraDisabled = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this._XForm_Camera = this.transform;
+        this._XFORM_Parent = this.transform.parent;
+
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CameraDisabled = !CameraDisabled;
+        }
+
+        Cursor.visible = CameraDisabled;
+
+        if (!CameraDisabled)
+        {
+            
+
+            if(Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+            {
+
+                LocalRotation.x += Input.GetAxis("Mouse X") * MouseSensivity;
+                LocalRotation.y -= Input.GetAxis("Mouse Y") * MouseSensivity;
+
+                LocalRotation.y = Mathf.Clamp(LocalRotation.y, 0f, 90f);
+
+            }
+
+
+        }
+
+        if(Input.GetAxis("Mouse ScrollWheel") != 0f)
+            {
+                float ScrollAmount = Input.GetAxis("Mouse ScrollWheel") * ScrollSensivity;
+
+                ScrollAmount *= (this._CameraDistance * 0.3f);
+
+                this._CameraDistance += ScrollAmount * -1f;
+
+                this._CameraDistance = Mathf.Clamp(this._CameraDistance, 1.5f, 100f);
+
+            }
+
+        Quaternion QT = Quaternion.Euler(LocalRotation.y, LocalRotation.x, 0);
+        this._XFORM_Parent.rotation = Quaternion.Lerp(this._XFORM_Parent.rotation, QT, Time.deltaTime * OrbitDampening);
+
+        if(this._XForm_Camera.localPosition.z != this._CameraDistance * -1f)
+        {
+            this._XForm_Camera.localPosition = new Vector3(0f, 0f, Mathf.Lerp(this._XForm_Camera.localPosition.z, this._CameraDistance * -1f, Time.deltaTime * ScrollDampening));
+        }
+    }
+}
---
+++ Assets/Scripts/MouseDetect.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using TMPro;
+using UnityEngine;
+
+public class MouseDetect : MonoBehaviour
+{
+
+    public TextMeshProUGUI tmpg;
+    public GameObject gameobject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmpg.text = "Hover over any tool for information";
+    }
+
+    private void OnMouseEnter()
+    {
+
+        if (Cursor.visible)
+        {
+            tmpg.text = returnDescByID(gameobject.name);
+        }
+
+        Debug.Log(Cursor.visible);
+        Debug.Log(returnDescByID(gameobject.name));
+    }
+
+    public void OnMouseExit()
+    {
+
+            tmpg.text = "";
+        
+    }
+
+    public string returnDescByID(string gameObjName)
+    {
+        string desc;
+
+        switch (gameObjName) {
+
+            case "BHohlstrahlrohr":
+                desc = "<size=30%>Fog Nozzle Size B\n<size=25%>Flow Rate: 215l/min\n<size=25%>Reach:28m";
+                break;
+
+            case "BHohlstrahlrohr2":
+                desc = "<size=30%>Fog Nozzle Size B\n<size=25%>Flow Rate: 215l/min\n<size=25%>Reach:28m";
+                break;
+
+            case "Body1":
+                desc = "<size=30%>Fire Hose Case\n<size=25%>Contains up to 3 C-sized fire hoses";
+                break;
+
+            case "Key":
+                desc = "<size=30%>Hydrant key\n<size=25%>Is used to open surface level hydrants";
+                break;
+
+            case "Ladder":
+                desc = "<size=30%>Ladder\n<size=25%>Is used to access buildings through windows, or in rare cases build containment pools";
+                break;
+
+            case "CStrahl1":
+                desc = "<size=30%>Fog Nozzle Size C\n<size=25%>Flow Rate: 150l/min\n<size=25%>Reach:27m";
+                break;
+
+            case "CStrahl2":
+                desc = "<size=30%>Fog Nozzle Size C\n<size=25%>Flow Rate: 150l/min\n<size=25%>Reach:27m";
+                break;
+
+            case "Druckbegrenzung":
+                desc = "<size=30%>Pressure relief valve\n<size=25%>Prevents fire hoses from bursting of shock which occurs when a nozzle is closed too quickly";
+                break;
+
+            case "Druckbegrenzung2":
+                desc = "<size=30%>Pressure relief valve\n<size=25%>Prevents fire hoses from bursting of shock which occurs when a nozzle is closed too quickly";
+                break;
+
+            case "idek":
+                desc = "<size=30%>Pressure control unit\n<size=25%>Controls internal water pump pressure";
+                break;
+
+            case "Kugelhahn1":
+                desc = "<size=30%>Ball valve\n<size=25%>Most commonly used directly at hydrants to regulate shock";
+                break;
+
+            case "Kugelhahn2":
+                desc = "<size=30%>Ball valve\n<size=25%>Most commonly used directly at hydrants to regulate shock";
+                break;
+
+            case "mth":
+                desc = "<size=30%>Converter\n<size=25%>Converts size B mounts to size C";
+                break;
+
+            case "mth2":
+                desc = "<size=30%>Converter\n<size=25%>Converts size B mounts to size C";
+                break;
+
+            case "QuickAccess1":
+                desc = "<size=30%>Internal water storage access\n<size=25%>Used when there is no immediate source of water, and as a buffer";
+                break;
+
+            case "QuickAccess2":
+                desc = "<size=30%>Internal water storage access\n<size=25%>Used when there is no immediate source of water, and as a buffer";
+                break;
+
+            case "Schlauchhalter":
+                desc = "<size=30%>Hose pins\n<size=25%>A box of pieces that can be used to tie fire hoses to fences, poles, or similar obstacles.";
+                break;
+
+            case "Sttzkrmmer":
+                desc = "<size=30%>Branchpipe holder\n<size=25%>Most commonly used to connect to size B fire nozzles without a pistol grip for improved ergonomics";
+                break;
+
+            case "Sttzkrmmer2":
+                desc = "<size=30%>Branchpipe holder\n<size=25%>Most commonly used to connect to size B fire nozzles without a pistol grip for improved ergonomics";
+                break;
+
+            default:
+                desc = "no value assigned";
+                break;
+        }
+
+        return desc;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
